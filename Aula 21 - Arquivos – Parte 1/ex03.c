180eb80d6306df4b92f99dc5d60d25e53229c852
Paludeto <gpalu54@gmail.com>
2024-05-17 21:36:55 -0300

    enunciado ex03.c

--- Aula 21 - Arquivos – Parte 1/ex03.c
+++ Aula 21 - Arquivos – Parte 1/ex03.c
@@ -1,7 +1,8 @@
 /**
  * @file ex03.c
  * @author Gabriel Paludeto 
- * @brief 
+ * @brief 3) Escreva um programa para converter o conteúdo de um arquivo texto em caracteres maiúsculos. 
+ * O programa deverá ler do usuário o nome do arquivo a ser convertido e o nome do arquivo a ser salvo.
  * @version 0.1
  * @date 2024-05-17
  * 
